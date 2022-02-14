129378fbcc657a76b44237e600b3ca8f6f5e613b
Stefano Corra <stefano.corra@hotmail.com>
2022-02-13 21:10:13 -0500

    use-state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import StarRating from "./components/StarRating";
 
 
 // function App() {
@@ -28,30 +29,30 @@ import { FaStar } from "react-icons/fa";
 //   );
 // }
 
-const createArray = (length) => [...Array(length)];
+// const createArray = (length) => [...Array(length)];
 
-const Star = ({selected=false,onSelect}) => {
-  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
-};
+// const Star = ({selected=false,onSelect}) => {
+//   return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
+// };
 
-const StarRating = ({ totalStars = 5 }) => {
+// const StarRating = ({ totalStars = 5 }) => {
   
-  const [selectedStars, setSelectedStars] = useState(0);
+//   const [selectedStars, setSelectedStars] = useState(0);
 
-  return (
-    <>
-    {
-      createArray(totalStars).map((n, i) => (
-    <Star
-      key={i}
-      selected={selectedStars > i}
-      onSelect={() => setSelectedStars(i + 1)}
-    />
-      ))}
-      <p>{selectedStars} of { totalStars}</p>
-    </>
-  );
-};
+//   return (
+//     <>
+//     {
+//       createArray(totalStars).map((n, i) => (
+//     <Star
+//       key={i}
+//       selected={selectedStars > i}
+//       onSelect={() => setSelectedStars(i + 1)}
+//     />
+//       ))}
+//       <p>{selectedStars} of { totalStars}</p>
+//     </>
+//   );
+// };
 
 const App = () => {
   return <StarRating totalStars={5} />;
---
+++ src/components/StarRating.js
@@ -0,0 +1,33 @@
+import { useState } from "react/cjs/react.development";
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [...Array(length)];
+
+const Star = ({ selected = false, onSelect }) => {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
+};
+
+const StarRate = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
+};
+
+const StarRating = () => {
+  return <StarRate totalStars={5} />;
+};
+
+export default StarRating;
